7296201685da134b9bdd0540c474889dc3e13eb9
Iran Ferreira <iranferreira712@gmail.com>
2024-03-19 20:18:12 -0300

    feat: change in variable name

--- src/modules/user/repositories/user.repository.ts
+++ src/modules/user/repositories/user.repository.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from "../entities/user.entity";
 
 export interface UserRepository {
-    create(name: string, email: string, password: string, accessName: string): Promise<UserEntity>
+    create(name: string, email: string, password: string, accessId: string): Promise<UserEntity>
     findById(id: string): Promise<UserEntity>
     find(): Promise<UserEntity[]>
     update(id: string, name: string, email: string, password: string): Promise<void>
