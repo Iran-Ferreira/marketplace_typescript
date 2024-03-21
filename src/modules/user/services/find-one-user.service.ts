ac181040aed6694d9701e6e48618d4f3acd76e68
Iran Ferreira <iranferreira712@gmail.com>
2024-03-21 11:30:18 -0300

    feat: find one user service and controller

---
+++ src/modules/user/services/find-one-user.service.ts
@@ -0,0 +1,10 @@
+import { UserEntity } from '../entities/user.entity';
+import { UserRepository } from "../repositories/user.repository";
+
+export class FindOneUserService {
+    constructor(private readonly userRepo: UserRepository) {}
+    async execute(id: string): Promise<UserEntity> {
+        const user = await this.userRepo.findById(id)
+        return user
+    }
+}
